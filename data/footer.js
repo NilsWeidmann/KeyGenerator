d840a3a351d0ef747520f7b245bb08238309a931
NilsWeidmann <nils-w92@web.de>
2026-05-03 09:36:11 +0200

    Improve website appearance

---
+++ data/footer.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var footerHTML = `
+  <footer>
+    <p>
+      Kontakt: <a href="mailto:nils.weidmann@gmx.de">nils.weidmann@gmx.de</a>
+    </p>
+  </footer>`;
+  document.body.insertAdjacentHTML('beforeend', footerHTML);
+});
